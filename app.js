bcc704ae8ad76019d1f7ec7af5fc98d9e12e1fcd
KhyatiNaks <khyati.s@naksconsulting.com>
2021-11-02 17:44:41 +0530

    AD-19 filtering of data is being done properly and cleaned the code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const { log } = require("console");
 const express = require("express");
 const { sequelize, Media } = require("./models");
+const media = require("./models/media");
+const { isNullOrEmpty } = require("./utils");
 const app = express();
 app.use(express.json());
 
@@ -66,6 +69,7 @@ app.get("/media", async (req, res) => {
 //--------------------------
 app.get("/media", async (req, res) => {
   try {
+    var paramsString = "";
     // params
     // if (req.query.params)
     // {
@@ -73,56 +77,114 @@ app.get("/media", async (req, res) => {
     // findAll with parameters
 
     // }
-    var paramsString = "";
-    const searchParams = {
+    // var whereClause = "";
+    // if (Object.keys(req.query).length != 0) {
+    //   whereClause = {
+    //     site_code: req.query.site_code,
+    //     city_name: req.query.city_name,
+    //     location: req.query.location,
+    //   };
+    // }
+    const len = Object.keys(req.query).length;
+    paramsString = isNullOrEmpty(req.query.site_code)
+      ? req.query.site_code
+      : "" + " " + isNullOrEmpty(req.query.city_name)
+      ? req.query.city_name
+      : "" + " " + isNullOrEmpty(req.query.location)
+      ? req.query.location
+      : "";
+
+    console.log("I am the string of concatenated parameters" + paramsString);
+
+    // const whereClause = {
+    //   where: [
+    //     // sequelize.literal(
+    //     `searchableColumns @@to_tsquery(` + paramsString + `)`,
+    //     // ),
+    //   ],
+    // };
+
+    // const getMedia = await Media.findAll(len != 0 ? whereClause : {});
+
+    // var sql = `searchableColumns @@to_tsquery(` + paramsString + `)`;
+    // console.log("query before execution  :  " + sql);
+    // // var params = { search_term: search_term };
+
+    // const getMedia = await sequelize
+    //   .query(sql, media, null, paramsString)
+    //   .then(function (products) {
+    //     // handle your products here
+    //     console.log("PRODUCTS   " + products);
+    //     process.exit();
+    //     // return res.jsonproducts;
+    //   });
+
+    const getMedia = await Media.findAll( len != 0 ? { where: {
       site_code: req.query.site_code,
       city_name: req.query.city_name,
       location: req.query.location,
-    };
+    } } : {})
+
+    return res.json(getMedia);
+
+    // Model.findAll({where: { [ Sequalize.literal (‘searchableColumns @@to_tsquery( <<enter your concatenated variable here> )’]}
+    // var paramsString = "";
+    // const searchParams = {
+    //   site_code: req.query.site_code,
+    //   city_name: req.query.city_name,
+    //   location: req.query.location,
+    // };
+    // console.log("checkme  " + Object.keys(req.query).length);
+
     // paramsString =
     //   searchParams.site_code +
-    //   " " +
+    //   "" +
     //   searchParams.city_name +
-    //   " " +
+    //   "" +
     //   searchParams.location;
-    // console.log(searchParams.site_code+"sp");
-    if (paramsString != null) {
-      // if (
-      //   searchParams.site_code != "" ||
-      //   searchParams.city_name != "" ||
-      //   searchParams.location != ""
-      // )  // I tried to check the params directly as I thought maybe the string is not being validated properly
-      console.log("if");
-      //   console.log(paramsString);
-      console.log(
-        "site_code:" +
-          searchParams.site_code +
-          "city_name:" +
-          searchParams.city_name +
-          "location:" +
-          searchParams.location
-      );
-      const getFilteredMedia = await Media.findAll({
-        // where: {
-        //   params:{$or: [
-        //     // { site_code: searchParams.site_code },
-        //     { city_name: { $iLike: searchParams.city_name } },
-        //     { location: { $iLike: searchParams.location } },
-        //   ]}
-        // },   //  here I was trying to get data even when user passes only any one of the three parameters(by using the "or" operator), but I guess this is not the right way to use or operator)
+    // console.log(searchParams.site_code + "sp");
+    // console.log("length    " + paramsString.length);
+    // console.log(paramsString);
+    // if (Object.keys(req.query).length != 0) {
+    //   console.log(Media.searchableColumn);
 
-        where: {
-          site_code: searchParams.site_code,
-          city_name: searchParams.city_name,
-          location: searchParams.location,
-        }
-      });
-      return res.json(getFilteredMedia);
-    } else {
-      console.log("else");
-      const getAllMedia = await Media.findAll(); //pass a where clause if the string you got above is not ''
-      return res.json(getAllMedia);
-    }
+    //   // if (
+    //   //   searchParams.site_code != "" ||
+    //   //   searchParams.city_name != "" ||
+    //   //   searchParams.location != ""
+    //   // )  // I tried to check the params directly as I thought maybe the string is not being validated properly
+    //   console.log("if");
+    //   //   console.log(paramsString);
+    //   console.log(
+    //     "site_code:" +
+    //       searchParams.site_code +
+    //       "city_name:" +
+    //       searchParams.city_name +
+    //       "location:" +
+    //       searchParams.location
+    //   );
+    //   const getFilteredMedia = await Media.findAll({
+    //     // where: {
+    //     //   params:{$or: [
+    //     //     // { site_code: searchParams.site_code },
+    //     //     { city_name: { $iLike: searchParams.city_name } },
+    //     //     { location: { $iLike: searchParams.location } },
+    //     //   ]}
+    //     // },   //  here I was trying to get data even when user passes only any one of the three parameters(by using the "or" operator), but I guess this is not the right way to use or operator)
+
+    //     where: {
+    //       site_code: searchParams.site_code,
+    //       city_name: searchParams.city_name,
+    //       location: searchParams.location,
+    //     },
+    //   });
+    //   return res.json(getFilteredMedia);
+    // } else {
+    //   console.log(paramsString);
+    //   console.log("else");
+    //   const getAllMedia = await Media.findAll(); //pass a where clause if the string you got above is not ''
+    //   return res.json(getAllMedia);
+    // }
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Something went wrong" });
