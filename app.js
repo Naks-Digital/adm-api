6e98623c797c73a4639f127934050cc5caccf00a
KhyatiNaks <khyati.s@naksconsulting.com>
2021-11-03 18:08:22 +0530

    AD-19 Modified for passing parameters in the API calls

--- app.js
+++ app.js
@@ -56,135 +56,41 @@ app.post("/media", async (req, res) => {
     return res.status(500).json(err);
   }
 });
-
-app.get("/media", async (req, res) => {
-  try {
-    const getAllMedia = await Media.findAll();
-    return res.json(getAllMedia);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-});
-//--------------------------
 app.get("/media", async (req, res) => {
   try {
     var paramsString = "";
-    // params
-    // if (req.query.params)
-    // {
-    // concatenate all the params and storer it in a string
-    // findAll with parameters
-
-    // }
-    // var whereClause = "";
-    // if (Object.keys(req.query).length != 0) {
-    //   whereClause = {
-    //     site_code: req.query.site_code,
-    //     city_name: req.query.city_name,
-    //     location: req.query.location,
-    //   };
-    // }
+    var sql = "";
+    var modifiedSiteCode = "";
     const len = Object.keys(req.query).length;
-    paramsString = isNullOrEmpty(req.query.site_code)
-      ? req.query.site_code
-      : "" + " " + isNullOrEmpty(req.query.city_name)
-      ? req.query.city_name
-      : "" + " " + isNullOrEmpty(req.query.location)
-      ? req.query.location
-      : "";
-
+    if (len != 0) {
+      const rem = req.query.site_code.toString();
+      console.log("djhefdgjfsb" + rem);
+      modifiedSiteCode = rem.split(" ").join("&");
+      console.log(rem.split(" ").join("&"));
+      paramsString =
+        (isNullOrEmpty(modifiedSiteCode) ? modifiedSiteCode : "") +
+        "&" +
+        (isNullOrEmpty(req.query.city_name) ? req.query.city_name : "") +
+        "&" +
+        (isNullOrEmpty(req.query.location) ? req.query.location : "");
+      // sql =
+      //   `select state_name, traffic_movement , post_code from media where searchableColumns @@to_tsquery('` +
+      //   paramsString +
+      //   `')`;
+      sql =
+        `select * from media where searchableColumns @@to_tsquery('` +
+        paramsString +
+        `')`;
+    } else {
+      sql = `select * from media`;
+    }
     console.log("I am the string of concatenated parameters" + paramsString);
 
-    // const whereClause = {
-    //   where: [
-    //     // sequelize.literal(
-    //     `searchableColumns @@to_tsquery(` + paramsString + `)`,
-    //     // ),
-    //   ],
-    // };
-
-    // const getMedia = await Media.findAll(len != 0 ? whereClause : {});
-
-    // var sql = `searchableColumns @@to_tsquery(` + paramsString + `)`;
-    // console.log("query before execution  :  " + sql);
-    // // var params = { search_term: search_term };
-
-    // const getMedia = await sequelize
-    //   .query(sql, media, null, paramsString)
-    //   .then(function (products) {
-    //     // handle your products here
-    //     console.log("PRODUCTS   " + products);
-    //     process.exit();
-    //     // return res.jsonproducts;
-    //   });
-
-    const getMedia = await Media.findAll( len != 0 ? { where: {
-      site_code: req.query.site_code,
-      city_name: req.query.city_name,
-      location: req.query.location,
-    } } : {})
-
-    return res.json(getMedia);
-
-    // Model.findAll({where: { [ Sequalize.literal (‘searchableColumns @@to_tsquery( <<enter your concatenated variable here> )’]}
-    // var paramsString = "";
-    // const searchParams = {
-    //   site_code: req.query.site_code,
-    //   city_name: req.query.city_name,
-    //   location: req.query.location,
-    // };
-    // console.log("checkme  " + Object.keys(req.query).length);
-
-    // paramsString =
-    //   searchParams.site_code +
-    //   "" +
-    //   searchParams.city_name +
-    //   "" +
-    //   searchParams.location;
-    // console.log(searchParams.site_code + "sp");
-    // console.log("length    " + paramsString.length);
-    // console.log(paramsString);
-    // if (Object.keys(req.query).length != 0) {
-    //   console.log(Media.searchableColumn);
-
-    //   // if (
-    //   //   searchParams.site_code != "" ||
-    //   //   searchParams.city_name != "" ||
-    //   //   searchParams.location != ""
-    //   // )  // I tried to check the params directly as I thought maybe the string is not being validated properly
-    //   console.log("if");
-    //   //   console.log(paramsString);
-    //   console.log(
-    //     "site_code:" +
-    //       searchParams.site_code +
-    //       "city_name:" +
-    //       searchParams.city_name +
-    //       "location:" +
-    //       searchParams.location
-    //   );
-    //   const getFilteredMedia = await Media.findAll({
-    //     // where: {
-    //     //   params:{$or: [
-    //     //     // { site_code: searchParams.site_code },
-    //     //     { city_name: { $iLike: searchParams.city_name } },
-    //     //     { location: { $iLike: searchParams.location } },
-    //     //   ]}
-    //     // },   //  here I was trying to get data even when user passes only any one of the three parameters(by using the "or" operator), but I guess this is not the right way to use or operator)
-
-    //     where: {
-    //       site_code: searchParams.site_code,
-    //       city_name: searchParams.city_name,
-    //       location: searchParams.location,
-    //     },
-    //   });
-    //   return res.json(getFilteredMedia);
-    // } else {
-    //   console.log(paramsString);
-    //   console.log("else");
-    //   const getAllMedia = await Media.findAll(); //pass a where clause if the string you got above is not ''
-    //   return res.json(getAllMedia);
-    // }
+    const getMedia = await sequelize
+      .query(sql, media, null, paramsString)
+      .then(function (resultedData) {
+        return res.json(resultedData);
+      });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Something went wrong" });
